ff81d793e994eb8dd7918178e8c7c53a768a7d68
Emily Strickland <emily@zubon.org>
2011-05-29 19:15:13 -0700

    Broken version with initial canberra code

--- src/bell.cpp
+++ src/bell.cpp
@@ -1,6 +1,8 @@
 #include "bell.h"
 #include <core/atoms.h>
 
+#include <canberra.h>
+
 COMPIZ_PLUGIN_20090315 (bell, BellPluginVTable);
 
 void
@@ -12,12 +14,29 @@ AudibleBell::handleEvent (XEvent *event)
 
 		if (xkbEvent->xkb_type == XkbBellNotify)
 		{
-			/* XkbBellNotifyEvent *xkbBellEvent = (XkbBellNotifyEvent *) xkbEvent; */
+			XkbBellNotifyEvent *xkbBellEvent = (XkbBellNotifyEvent *) xkbEvent;
 
-			if (optionGetAudibleBell ())
+			/* if (optionGetAudibleBell ())
 			{
-				/* libcanberra magic here */
-			}
+			*/
+			    ca_context *c;
+                ca_proplist *p;
+                int res;
+
+                ca_proplist_create (&p);
+                ca_proplist_sets (p, CA_PROP_EVENT_ID, "bell-window-system");
+                ca_proplist_sets (p, CA_PROP_EVENT_DESCRIPTION, "Bell event");
+                ca_proplist_sets (p, CA_PROP_CANBERRA_CACHE_CONTROL, "permanent");
+                
+                ca_context_open (c);
+
+                /* First, we try to play a real sound ... */
+                res = ca_context_play_full (c, 1, p, NULL, NULL);
+
+                ca_proplist_destroy (p);
+                ca_context_destroy (c);
+
+			/* } */
 		}
     }
 }
